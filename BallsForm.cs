835d5035dcaf44ad2eb117d0a6103137beaa3851
m1nus0ne <voldemar7455@gmail.com>
2021-10-01 22:28:21 +0500

    init

---
+++ BallsForm.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace GravityBalls
+{
+	public class BallsForm : Form
+	{
+		private Timer timer;
+		private WorldModel world;
+
+		private WorldModel CreateWorldModel()
+		{
+			var w = new WorldModel
+			{
+				WorldHeight = ClientSize.Height,
+				WorldWidth = ClientSize.Width,
+				BallRadius = 40,
+				VelocityX = 0,
+				VelocityY = 0,
+				Resistance = 1.01,
+				Gravity = 0,
+				Forse = 1000
+
+
+
+			};
+			w.BallX = w.WorldHeight / 2;
+			w.BallY = w.BallRadius;
+			
+			return w;
+		}
+
+		public double[] MouseEventArgs { get; set; }
+
+		protected override void OnResize(EventArgs e)
+		{
+			base.OnResize(e);
+			world.WorldHeight = ClientSize.Height;
+			world.WorldWidth = ClientSize.Width;
+		}
+
+		protected override void OnLoad(EventArgs e)
+		{
+			base.OnLoad(e);
+			DoubleBuffered = true;
+			BackColor = Color.Black;
+			world = CreateWorldModel();
+			timer = new Timer { Interval = 5 };
+			timer.Tick += TimerOnTick;
+			timer.Start();
+			world.WorldHeight = ClientSize.Height;
+			world.WorldWidth = ClientSize.Width;
+		}
+
+		private void TimerOnTick(object sender, EventArgs eventArgs)
+		{
+			world.SimulateTimeframe(timer.Interval / 1000d);
+			Invalidate();
+		}
+
+		protected override void OnPaint(PaintEventArgs e)
+		{
+			base.OnPaint(e);
+			var g = e.Graphics;
+			g.SmoothingMode = SmoothingMode.HighQuality;
+			g.FillEllipse(Brushes.GreenYellow,
+				(float)(world.BallX - world.BallRadius),
+				(float)(world.BallY - world.BallRadius),
+				2 * (float)world.BallRadius,
+				2 * (float)world.BallRadius);
+		}
+
+		protected override void OnMouseMove(MouseEventArgs e)
+		{
+			base.OnMouseMove(e);
+			Text = string.Format("Cursor ({0}, {1})", e.X, e.Y);
+			world.MouseCordX = e.X;
+			world.MouseCordY = e.Y;
+		}
+	}
+}
---
+++ WorldModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace GravityBalls
+{
+    public class WorldModel
+    {
+        public double BallX;
+        public double BallY;
+        public double BallRadius;
+        public double WorldWidth;
+        public double WorldHeight;
+        public double VelocityX;
+        public double VelocityY;
+        public double Resistance;
+        public double Gravity;
+        public double MouseCordX;
+        public double MouseCordY;
+        public double Forse;
+
+
+        public void SimulateTimeframe(double dt)
+        {
+            double distance = Math.Sqrt(Math.Pow(MouseCordY - BallY, 2) + Math.Pow(MouseCordX - BallX, 2));
+            double angle = GetAngle(BallX, BallY, MouseCordX, MouseCordY); //Между положтельным направление Ox и вектором силы
+            
+            if (BallX - BallRadius <= 0 | BallX + BallRadius >= WorldWidth) VelocityX *= -1;
+            if (BallY - BallRadius <= 0 | BallY + BallRadius >= WorldHeight) VelocityY *= -1;
+            double PushForse = Forse / Math.Sqrt(Math.Pow(MouseCordY - BallY, 2) + Math.Pow(MouseCordX - BallX, 2));
+            VelocityX = VelocityX/Resistance + Math.Cos(angle)*PushForse;
+            VelocityY = VelocityY/Resistance + Gravity*dt + Math.Sin(angle)*PushForse;
+
+            BallY = BallY + VelocityY * dt;
+            BallX = BallX + VelocityX * dt;
+        }
+
+        public double GetAngle(double x1, double y1, double x2, double y2)
+        {
+            return Math.Atan((x1 - x2) / (y1 - y2));
+        }
+    }
+}
